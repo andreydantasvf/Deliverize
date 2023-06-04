f30409a97f6d04e247385a0735118d6d2270ed00
Andrey Dantas <andreydantasvf@gmail.com>
2023-06-04 10:26:38 -0300

    Add responsive layout desktop

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,6 +1,7 @@
-import { AiOutlineLeft } from "react-icons/ai";
+import { AiOutlineDown, AiOutlineLeft, AiOutlineShoppingCart } from "react-icons/ai";
+import { BiUserCircle } from "react-icons/bi";
 
-import { Container, Logo } from "./styles";
+import { Container, Logo, Nav, Address, Icon } from "./styles";
 
 export function Header() {
   return (
@@ -10,6 +11,26 @@ export function Header() {
       <Logo>
         <img src="/Deliverize.png" alt="Logo Deliverize" />
       </Logo>
+
+      <Nav>
+        <Address>
+          <span>Entrega:</span>
+          <AiOutlineDown />
+          R. Antonio Braune, 222
+        </Address>
+
+        <input type="text" placeholder="Busque por estabelecimento ou produtos" />
+
+        <Icon>
+          <BiUserCircle />
+          Entrar
+        </Icon>
+
+        <Icon>
+          <AiOutlineShoppingCart />
+          Carrinho
+        </Icon>
+      </Nav>
     </Container>
   )
 }
--- src/components/Header/styles.ts
+++ src/components/Header/styles.ts
@@ -12,6 +12,14 @@ export const Container = styled.header`
   > svg {
     width: 3rem;
   }
+
+  @media(min-width: 980px) {
+    padding: 1.6rem 4rem;
+    gap: 13rem;
+    > svg {
+      display: none;
+    }
+  }
 `;
 
 export const Logo = styled.div`
@@ -22,5 +30,81 @@ export const Logo = styled.div`
 
   > img {
     width: 22rem;
+  }
+
+  @media(min-width: 980px) {
+    width: fit-content;
+    justify-content: flex-start;
+  }
+`;
+
+export const Nav = styled.div`
+  display: none;
+
+  > input {
+    width: 100%;
+    padding: 2rem 1.6rem;
+
+    border: 1px solid #ED3237;
+    border-radius: .4rem;
+    background-color: #FFF;
+
+    &::placeholder {
+      color: #656363;
+      font-size: 1.4rem;
+      line-height: 1rem;
+    }
+  }
+
+  @media(min-width: 980px) {
+    width: 100%;
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    gap: 2.4rem;
+  }
+`;
+
+export const Address = styled.button`
+  width: 100%;
+  max-width: 27rem;
+  background: #FFF;
+  box-shadow: 0px 1px 2px rgba(0, 0, 0, 0.1);
+  border: none;
+  border-radius: .5rem;
+  padding: .8rem 1.4rem .8rem .8rem;
+
+  display: flex;
+  flex-direction: column;
+
+  font-size: 1.2rem;
+  line-height: 1.2rem;
+  font-weight: 700;
+
+  > span {
+    color: #D80000;
+    font-weight: 400;
+    line-height: 2.5rem;
+  }
+
+  > svg {
+    font-size: 1.2rem;
+    color: #D80000;
+    align-self: flex-end;
+  }
+`;
+
+export const Icon = styled.button`
+  background: none;
+  border: none;
+
+  display: flex;
+  align-items: center;
+  gap: 1.2rem;
+  color: #ED3237;
+  line-height: 1.7rem;
+
+  > svg {
+    font-size: 2.6rem;
   }
 `;
